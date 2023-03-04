f74172c9ac17e1444ec428d8106aa8de97b02024
BatLeDev <guerny.e2200228@etud.univ-ubs.fr>
2023-03-04 17:55:54 +0100

    shema relationnel

--- ShemaRelationnel.sql
+++ ShemaRelationnel.sql
@@ -1,5 +1,5 @@
 /*
- SAE Velos de nantes
+ SAE Velos de nantes - Shema relationnel
 
  =============== Auteurs ===============
  GUERNY Baptiste
@@ -10,8 +10,19 @@
 
  ==================== Shema relationnel ====================
  
+ Quartier(code(1), nom, longueurPiste)
+ Compteur(numero(1), libelle, longitude, latitude, @leQuartier = Quartier.code(UQ))
+ Jour(jour(1), jourDeSemaine(NN), vacancesZoneB)
+ ReleveJournalier(leCompteur=@Compteur.numero(1), leJour=@Jour.jour(1), heure0, heure1, ..., heure23, total, probabiliteAnomalie)
 
-
-
+ -- Contraintes textuelles
+    ReleveJournalier :
+		- heureX >= 0
+        - total >= 0
+	Quartier : 
+		- longueurPiste >= 0
+	Jour :
+		- 0 < jourDeSemaine <= 8
+		- DOM(vacancesZoneB) = { 'Hors Vacances', 'Pont de l'Ascension', 'Vacances d'été', 'Vacances de la Toussaint', 'Vacances de Noël', 'Vacances de printemps', 'Vacances d'hiver' }
 
 */
