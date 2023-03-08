e08a7f50606f130579c078b26ea3a078ec49c2cb
BatLeDev <guerny.e2200228@etud.univ-ubs.fr>
2023-03-08 12:32:53 +0100

    Convert to mysql

--- Python/generationScriptRemplissage.py
+++ Python/generationScriptRemplissage.py
@@ -2,7 +2,7 @@
 def remplissageQuartier():
     # On ouvre le fichier qui détaille les quartiers de Nantes
     file1 = open("./data/data_quartiers_nantes.csv")
-    content1 = file1.readlines() # Transforme le fichier en liste de lignes
+    content1 = file1.readlines()  # Transforme le fichier en liste de lignes
 
     # On ouvre le fichier qui détaille la longueur des pistes cyclables
     file2 = open("./data/data_longueur_pistes_velo.csv")
@@ -35,10 +35,11 @@ def remplissageQuartier():
 # Donc il faut parcourrir le premier fichier et on ajoute tous les compteurs dans un dictionnaire de la forme : {numero : [libelle, observations, longitude, latitude, NULL],...} Si observations est une chaine vide on met NULL
 # Ensuite, on parcours le deuxième fichier et on complète le dictionnaire avec le code du quartier, si le compteur n'est pas dans le dictionnaire on l'ajoute dans le dictionnaire sous cette forme : {numero : [NULL, NULL, NULL, NULL, leQuartier]}
 
+
 def remplissageCompteur():
     # On ouvre le fichier qui détaille les compteurs de Nantes
     file1 = open("./data/data_geolocalisationCompteur.csv")
-    content1 = file1.readlines() # Transforme le fichier en liste de lignes
+    content1 = file1.readlines()  # Transforme le fichier en liste de lignes
 
     # On ouvre le fichier qui fait le lien entre un compteur et un quartier
     file2 = open("./data/data_quartier_compteur.csv")
@@ -63,7 +64,7 @@ def remplissageCompteur():
 
         if observations == "":
             observations = "NULL"
-        else :
+        else:
             observations = "'" + observations + "'"
 
         compteur[numero] = [libelle, observations, longitude, latitude, "NULL"]
@@ -101,7 +102,7 @@ def remplissageCompteur():
     fileOutput.close()
 
 # Pour remplir la table Jour et la table ReleveJournalier il faut parcourir le fichier data_comptageVelo.csv qui détaille les relevés journaliers
-# Table Jour : jour, jourDeSemaine, vacancesZoneB
+# Table Jour : datejour, jourDeSemaine, vacancesZoneB
 # Table ReleveJournalier : leCompteur, leJour, heure0, heure1, heure2, heure3, heure4, heure5, heure6, heure7, heure8, heure9, heure10, heure11, heure12, heure13, heure14, heure15, heure16, heure17, heure18, heure19, heure20, heure21, heure22, heure23, total, probabiliteAnomalie
 # On parcours le fichier, on créé direct le fichier remplissageRelerJournalier.sql et on ajoute les jours dans un dictionnaire de la forme : {date : ,...} Si la date est déjà dans le dictionnaire on ne l'ajoute pas
 
@@ -109,44 +110,50 @@ def remplissageCompteur():
 def remplissageReleveJournalier_Jour():
     # On ouvre le fichier qui détaille les relevés journaliers des compteurs de Nantes
     file1 = open("./data/data_comptageVelo.csv")
-    content1 = file1.readlines() # Transforme le fichier en liste de lignes
+    content1 = file1.readlines()  # Transforme le fichier en liste de lignes
 
-    fileOutputReleveJournalier = open("./Scripts/remplissageReleveJournalier.sql", "w")
+    fileOutputReleveJournalier = open(
+        "./Scripts/remplissageReleveJournalier.sql", "w")
     fileOutputJour = open("./Scripts/remplissageJour.sql", "w")
 
+    fileOutputReleveJournalier.write("INSERT INTO ReleveJournalier VALUES\n")
     # On parcours le fichier
     jour = {}
     for i in range(1, len(content1)):
         lineF1 = content1[i].replace("\n", "").split(";")
-        numero = lineF1[0]
-        # libelle = ligneF1[1]
-        date = lineF1[2]
-        heures = {}
-        for j in range(3, 27):
-            if lineF1[j] == "":
-                heures[j-3] = "0"
+        if lineF1[28] != "Forte" and lineF1[1] != "":
+            numero = lineF1[0]
+            # libelle = ligneF1[1]
+            date = lineF1[2]
+            heures = {}
+            for j in range(3, 27):
+                if lineF1[j] == "" or int(lineF1[j]) < 0:
+                    heures[j-3] = "0"
+                else:
+                    heures[j-3] = lineF1[j]
+
+            total = lineF1[27]
+            if int(total) < 0:
+                total = "0"
+
+            if lineF1[28] == "":
+                probabiliteAnomalie = "NULL"
             else:
-                heures[j-3] = lineF1[j]
-            
-        total = lineF1[27]
-        if lineF1[28] == "":
-            probabiliteAnomalie = "NULL"
-        else:
-            probabiliteAnomalie = "'" + lineF1[28] + "'"
+                probabiliteAnomalie = "'" + lineF1[28] + "'"
 
+            # On écrit dans le fichier de sortie de la table ReleveJournalier
+            ligneOutput = "(" + numero + ",DATE '" + date + "',"
+            for heure in heures:
+                ligneOutput += heures[heure] + ","
+            ligneOutput += total + "," + probabiliteAnomalie + "),\n"
+            fileOutputReleveJournalier.write(ligneOutput)
 
-        # On écrit dans le fichier de sortie de la table ReleveJournalier
-        ligneOutput = "INSERT INTO ReleveJournalier VALUES (" + numero + ", DATE '" + date + "', "
-        for heure in heures:
-            ligneOutput += heures[heure] + ", "
-        ligneOutput += total + ", " + probabiliteAnomalie + ");\n"
-        fileOutputReleveJournalier.write(ligneOutput)
-
-        # On ajoute le jour dans le dictionnaire
-        if date not in jour:
-            jourDeSemaine = lineF1[29]
-            vacancesZoneB = lineF1[32].replace("'", "''") # On remplace les ' par des '' pour éviter les erreurs SQL
-            jour[date] = [jourDeSemaine, vacancesZoneB]
+            # On ajoute le jour dans le dictionnaire
+            if date not in jour:
+                jourDeSemaine = lineF1[29]
+                # On remplace les ' par des '' pour éviter les erreurs SQL
+                vacancesZoneB = lineF1[32].replace("'", "''")
+                jour[date] = [jourDeSemaine, vacancesZoneB]
 
     # On écrit dans le fichier de sortie de la table Jour
     for date in jour:
--- Scripts/creationTables.sql
+++ Scripts/creationTables.sql
@@ -9,96 +9,81 @@
  PIERRE Noé
  
  ==================== Script de création des tables ====================
-*/
-
--- ===== Surpession des tables =====
-DROP TABLE ReleveJournalier;
-DROP TABLE Jour;
-DROP TABLE Compteur;
-DROP TABLE Quartier;
+ */
+-- ===== Suppression des tables =====
+DROP TABLE IF EXISTS ReleveJournalier;
+DROP TABLE IF EXISTS Jour;
+DROP TABLE IF EXISTS Compteur;
+DROP TABLE IF EXISTS Quartier;
 
 -- ===== Création des tables =====
-
 CREATE TABLE Quartier (
-    code NUMBER CONSTRAINT pk_Quartier PRIMARY KEY,
-    nom VARCHAR2(100),
-    longueurPiste NUMBER
+    code INT PRIMARY KEY,
+    nom VARCHAR(100),
+    longueurPiste INT
 );
 
 CREATE TABLE Compteur (
-    numero NUMBER CONSTRAINT pk_Compteur PRIMARY KEY,
-    libelle VARCHAR2(100),
-    observations VARCHAR2(100),
-    longitude DECIMAL(18,15),
+    numero INT PRIMARY KEY,
+    libelle VARCHAR(100),
+    observations VARCHAR(100),
+    longitude DECIMAL(18, 15),
     latitude DECIMAL(18, 15),
-    leQuartier NUMBER CONSTRAINT fk_Compteur_Quartier REFERENCES Quartier(code)
+    leQuartier INT,
+    CONSTRAINT fk_Compteur_Quartier FOREIGN KEY (leQuartier) REFERENCES Quartier(code)
 );
 
 CREATE TABLE Jour (
-    jour DATE CONSTRAINT pk_Jour PRIMARY KEY,
-    jourDeSemaine NUMBER
-        CONSTRAINT ck_jourDeSemaine CHECK (jourDeSemaine >= 1 AND jourDeSemaine <= 7) 
-        CONSTRAINT nn_jourDeSemaine NOT NULL,
-    vacancesZoneB VARCHAR2(100)
-        CONSTRAINT ck_vacancesZoneB CHECK (vacancesZoneB IN ('Hors Vacances', 'Pont de l''Ascension', 'Vacances d''été', 'Vacances de la Toussaint', 'Vacances de Noël', 'Vacances de printemps', 'Vacances d''hiver'))
+    datejour DATE PRIMARY KEY,
+    jourDeSemaine INT CHECK (
+        jourDeSemaine >= 1
+        AND jourDeSemaine <= 7
+    ),
+    vacancesZoneB VARCHAR(100) CHECK (
+        vacancesZoneB IN (
+            'Hors Vacances',
+            'Pont de l''Ascension',
+            'Vacances d''été',
+            'Vacances de la Toussaint',
+            'Vacances de Noël',
+            'Vacances de printemps',
+            'Vacances d''hiver'
+        )
+    )
 );
 
 CREATE TABLE ReleveJournalier (
-    leCompteur NUMBER 
-        CONSTRAINT fk_ReleveJournalier_Compteur REFERENCES Compteur(numero),
-    leJour DATE
-        CONSTRAINT fk_ReleveJournalier_Jour REFERENCES Jour(jour),
-    heure0 NUMBER
-        CONSTRAINT ck_heure0 CHECK (heure0 >= 0),
-    heure1 NUMBER
-        CONSTRAINT ck_heure1 CHECK (heure1 >= 0),
-    heure2 NUMBER
-        CONSTRAINT ck_heure2 CHECK (heure2 >= 0),
-    heure3 NUMBER
-        CONSTRAINT ck_heure3 CHECK (heure3 >= 0),
-    heure4 NUMBER
-        CONSTRAINT ck_heure4 CHECK (heure4 >= 0),
-    heure5 NUMBER
-        CONSTRAINT ck_heure5 CHECK (heure5 >= 0),
-    heure6 NUMBER
-        CONSTRAINT ck_heure6 CHECK (heure6 >= 0),
-    heure7 NUMBER
-        CONSTRAINT ck_heure7 CHECK (heure7 >= 0),
-    heure8 NUMBER
-        CONSTRAINT ck_heure8 CHECK (heure8 >= 0),
-    heure9 NUMBER
-        CONSTRAINT ck_heure9 CHECK (heure9 >= 0),
-    heure10 NUMBER
-        CONSTRAINT ck_heure10 CHECK (heure10 >= 0),
-    heure11 NUMBER
-        CONSTRAINT ck_heure11 CHECK (heure11 >= 0),
-    heure12 NUMBER
-        CONSTRAINT ck_heure12 CHECK (heure12 >= 0),
-    heure13 NUMBER
-        CONSTRAINT ck_heure13 CHECK (heure13 >= 0),
-    heure14 NUMBER
-        CONSTRAINT ck_heure14 CHECK (heure14 >= 0),
-    heure15 NUMBER
-        CONSTRAINT ck_heure15 CHECK (heure15 >= 0),
-    heure16 NUMBER
-        CONSTRAINT ck_heure16 CHECK (heure16 >= 0),
-    heure17 NUMBER
-        CONSTRAINT ck_heure17 CHECK (heure17 >= 0),
-    heure18 NUMBER
-        CONSTRAINT ck_heure18 CHECK (heure18 >= 0),
-    heure19 NUMBER
-        CONSTRAINT ck_heure19 CHECK (heure19 >= 0),
-    heure20 NUMBER
-        CONSTRAINT ck_heure20 CHECK (heure20 >= 0),
-    heure21 NUMBER
-        CONSTRAINT ck_heure21 CHECK (heure21 >= 0),
-    heure22 NUMBER
-        CONSTRAINT ck_heure22 CHECK (heure22 >= 0),
-    heure23 NUMBER
-        CONSTRAINT ck_heure23 CHECK (heure23 >= 0),
-    total NUMBER
-        CONSTRAINT ck_total CHECK (total >= 0),
-    probabiliteAnomalie VARCHAR2(10)
-        CONSTRAINT ck_probabiliteAnomalie CHECK (probabiliteAnomalie IN ('Faible', 'Moyenne', 'Forte')),
-    CONSTRAINT pk_ReleveJournalier PRIMARY KEY (leCompteur, leJour)
+    leCompteur INT,
+    leJour DATE,
+    heure0 INT CHECK (heure0 >= 0),
+    heure1 INT CHECK (heure1 >= 0),
+    heure2 INT CHECK (heure2 >= 0),
+    heure3 INT CHECK (heure3 >= 0),
+    heure4 INT CHECK (heure4 >= 0),
+    heure5 INT CHECK (heure5 >= 0),
+    heure6 INT CHECK (heure6 >= 0),
+    heure7 INT CHECK (heure7 >= 0),
+    heure8 INT CHECK (heure8 >= 0),
+    heure9 INT CHECK (heure9 >= 0),
+    heure10 INT CHECK (heure10 >= 0),
+    heure11 INT CHECK (heure11 >= 0),
+    heure12 INT CHECK (heure12 >= 0),
+    heure13 INT CHECK (heure13 >= 0),
+    heure14 INT CHECK (heure14 >= 0),
+    heure15 INT CHECK (heure15 >= 0),
+    heure16 INT CHECK (heure16 >= 0),
+    heure17 INT CHECK (heure17 >= 0),
+    heure18 INT CHECK (heure18 >= 0),
+    heure19 INT CHECK (heure19 >= 0),
+    heure20 INT CHECK (heure20 >= 0),
+    heure21 INT CHECK (heure21 >= 0),
+    heure22 INT CHECK (heure22 >= 0),
+    heure23 INT CHECK (heure23 >= 0),
+    total INT CHECK (total >= 0),
+    probabiliteAnomalie VARCHAR(10) CHECK (
+        probabiliteAnomalie IN ('Faible', 'Moyenne', 'Forte')
+    ),
+    CONSTRAINT pk_ReleveJournalier PRIMARY KEY (leCompteur, leJour),
+    CONSTRAINT fk_ReleveJournalier_Compteur FOREIGN KEY (leCompteur) REFERENCES Compteur(numero),
+    CONSTRAINT fk_ReleveJournalier_Jour FOREIGN KEY (leJour) REFERENCES Jour(datejour)
 );
